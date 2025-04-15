ad92ed51b011dca34ea6061293caee02c6591212
s.uchihori <s_uchihori@a-ssign.com>
2025-04-15 15:32:36 +0900

    fix: bugs

--- tools/todo/repository/delete-todo.ts
+++ tools/todo/repository/delete-todo.ts
@@ -10,17 +10,59 @@ export type DeleteTodoError = {
   cause?: unknown;
 };
 
+import {
+  getCompletionIndexListKey,
+  getContextIndexListKey,
+  getPriorityIndexListKey,
+  getProjectIndexListKey,
+} from "../utils/get-kv-key.ts";
+import { getTodoById } from "./get-todo-by-id.ts";
+
 export const deleteTodo = async (
   kv: Deno.Kv,
   id: string,
 ): Promise<Result<DeleteTodoSuccess, DeleteTodoError>> => {
   try {
-    // Delete the todo
-    await kv.delete([todoKey, id]);
+    const getTodoResult = await getTodoById(kv, id);
 
-    return ok({
-      id,
-    });
+    if (!getTodoResult.ok) {
+      return err({
+        message: `Todo with id ${id} not found`,
+      });
+    }
+
+    const todo = getTodoResult.data.todo;
+
+    const atomic = kv.atomic();
+
+    // Delete the todo
+    atomic.delete([todoKey, id]);
+
+    // Delete project indexes
+    for (const project of todo.projects) {
+      atomic.delete([...getProjectIndexListKey(project), id]);
+    }
+
+    // Delete context indexes
+    for (const context of todo.contexts) {
+      atomic.delete([...getContextIndexListKey(context), id]);
+    }
+
+    // Delete priority index
+    atomic.delete([...getPriorityIndexListKey(todo.priority), id]);
+
+    // Delete completion index
+    atomic.delete([...getCompletionIndexListKey(todo.completed), id]);
+
+    const result = await atomic.commit();
+
+    if (!result.ok) {
+      return err({
+        message: "Failed to commit transaction",
+      });
+    }
+
+    return ok({ id });
   } catch (error) {
     return err({
       message: "Unexpected error occurred while deleting todo",
